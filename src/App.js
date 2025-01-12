db6ccde00b387fb0c7648afc51224e3353c1c66e
Snezhana Petlova <snpetlova@gmail.com>
2025-01-12 12:43:04 +0200

    adjust styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> Memory Game </h1>
+      <p className="description"> Flip two cards to find a matching pair. If the cards match, they will remain flipped. If they don't, they will flip back after a short delay. Try to match all pairs in the least amount of moves. Good luck! </p>
       <Cards />
     </div>
   );
